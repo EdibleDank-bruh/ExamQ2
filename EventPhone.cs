495961c044ff3ad813552fa821c7a187f20c567b
Menbere Akele KIBRET <menbereakele@gmail.com>
2023-12-18 17:48:54 -0500

    Add project files.

---
+++ EventPhone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamQ2
+{
+    public class EventPhone : EventArgs
+    {
+        public string Message { get; set; }
+        public EventPhone(string message)
+        {
+            Message = message;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿namespace ExamQ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var em = new Emergency();
+            var boss = new Person("Menb", "4372613190");
+
+            em.EventEmergency += boss.EmergencyHappen;
+            em.Emergencyhappen("fire");
+
+
+        }
+        public class Person
+        {
+            public string Name { get; set; }
+            public string Phone { get; set; }
+            public Person(string name, string phone)
+            {
+                Name = name;
+                Phone = phone;
+            }
+            public void EmrgancyHappen(object sender, EventPhone e)
+            {
+                Console.WriteLine("calling " + e.Messgae);
+            }
+
+            internal void EmergencyHappen(object? sender, EventPhone e)
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public class Emergency
+        {
+            public event EventHandler<EventPhone> EventEmergency;
+            public void Emergencyhappen(string message)
+            {
+                EmrgancyCall(message);
+            }
+            public void EmrgancyCall(string message)
+            {
+                if (EventEmergency != null)
+                {
+                    EventEmergency.Invoke(this, new EventPhone(message));
+
+
+                }
+            }
+        }
+
+
+
+    }
+}
+}
